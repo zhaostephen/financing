2e65722d6927edcaa869df1cf7dae071f990670b
stephen.zhao <stephen.zhao@prcm.com>
2016-10-24 17:52:53 +0800

    typings

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-var app = require('express')();
+var http = require('http');
+var express = require('express');
+var app = express();
 var routine = require('./server/route.js')(app);
 
 var config = {
@@ -7,6 +9,23 @@ var config = {
 }
 
 console.log(config);
+
+app.get('/api/stock/:id', function(req, res){
+    console.log('received '+req.url);
+
+    if(req.params.id==100){
+        res.json({id:100, code:'000001.sh'});
+    }
+    else{
+        res.end();
+    }
+});
+
+app.use(function(req, res){
+    res.status(404);
+    res.type('text/plain');
+    res.send('404 - Not Found');
+});
 
 app.listen(config.port, function(req, res){
     console.log('started');
--- server/route.js
+++ server/route.js
@@ -1,18 +1,3 @@
 module.exports = function(app){
-    app.get('/api/stock/:id', function(req, res){
-        console.log('received '+req.url);
 
-        if(req.params.id==100){
-            res.json({id:100, code:'000001.sh'});
-        }
-        else{
-            res.end();
-        }
-    })
-
-    app.use(function(req, res){
-        res.status(404);
-        res.type('text/plain');
-        res.send('404 - Not Found');
-    });
 }
